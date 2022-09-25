1b524fe6ecc91cef1f4e5b055f2226a12950e4c0
Pradeep <kpradeep12@users.noreply.github.com>
2022-09-25 19:25:37 -0400

    updated files

--- src/main/java/org/algorithms/graph/Graph.java
+++ src/main/java/org/algorithms/graph/Graph.java
@@ -1,22 +1,24 @@
 package org.algorithms.graph;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.stream.IntStream;
 
 public class Graph {
     
     ArrayList<ArrayList<Integer>> list = new ArrayList<>();
     Integer size;
-    public Graph(Integer size) {
+    Boolean isDirected;
+    public Graph(Integer size, Boolean isDirected) {
         this.size = size;
+        this.isDirected = isDirected;
         IntStream.range(0, size)
             .forEach(n -> list.add(n, new ArrayList<>()));
     }
 
     public void addNode(Integer source, Integer destination) {
         list.get(source).add(destination);
-        list.get(destination).add(source);
+        if(!isDirected)
+            list.get(destination).add(source);
     }
 
     public void print() {
@@ -29,7 +31,7 @@ public class Graph {
     }
 
     public static void main(String[] args) {
-        Graph graph = new Graph(4);
+        Graph graph = new Graph(4, true);
         graph.addNode(0, 1);
         graph.addNode(0, 2);
         graph.addNode(2, 3);
