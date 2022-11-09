dbcedd52e4326eff2fae1b3b5814fc56ff781ac2
Pradeep <kpradeep12@users.noreply.github.com>
2022-11-08 20:25:47 -0500

    update

--- src/main/java/org/algorithms/ArrayProblems.java
+++ src/main/java/org/algorithms/ArrayProblems.java
@@ -36,8 +36,27 @@ public class ArrayProblems {
         }
         return result;
     }
+    public static boolean containsDuplicate(int[] input) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i <input.length; i++) {
+            if(map.containsKey(input[i])) return true;
+            map.put(input[i], 1);
+        }
+        return false;
+    }
+    public static int[] productExceptSelf(int[] input) {
+        int[] pa = new int[input.length]; pa[0] = 1;
+        int[] sa = new int[input.length]; sa[input.length-1] = 1;
+        int[] result = new int[input.length];
+        for(int i=1;i<pa.length;i++) pa[i] = pa[i-1] * input[i-1];
+        for(int i=sa.length-2;i>=0;i--) sa[i] = sa[i+1] * input[i+1];
+        for(int i=0;i<pa.length;i++) result[i] = pa[i] * sa[i];
+        return result;
+    }
     public static void main(String[] args) {
         //System.out.println(Arrays.toString(ArrayProblems.twoSumsWithMap(new int[]{11,8,3,2,15,7,10}, 9)));
-        System.out.println(bestTimeToBuy(new int[]{7,6,4,3,1}));
+        //System.out.println(bestTimeToBuy(new int[]{7,6,4,3,1}));
+        //System.out.println(containsDuplicate(new int[]{1,2,3,4,3}));
+        System.out.println(Arrays.toString(productExceptSelf(new int[]{1,2,3,4})));
     }
 }
