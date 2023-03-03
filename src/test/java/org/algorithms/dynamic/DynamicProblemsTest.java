5bb67f58f2bac52b29c6a11e4bf62deb50f744ec
Pradeep <kpradeep12@users.noreply.github.com>
2023-03-02 22:11:38 -0500

    update

--- src/test/java/org/algorithms/dynamic/DynamicProblemsTest.java
+++ src/test/java/org/algorithms/dynamic/DynamicProblemsTest.java
@@ -1,15 +1,16 @@
 package org.algorithms.dynamic;
 
-import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.algorithms.dynamic.DynamicProblems.*;
+import org.junit.Test;
 
 public class DynamicProblemsTest {
     @Test
     public void testFibonacciOf() {
-        assertEquals(0, DynamicProblems.fibonacciOf(0));
-        assertEquals(1, DynamicProblems.fibonacciOf(1));
-        assertEquals(1, DynamicProblems.fibonacciOf(2));
-        assertEquals(2, DynamicProblems.fibonacciOf(3));
-        assertEquals(8, DynamicProblems.fibonacciOf(6));
+        assertEquals(0, fibonacciOf(0));
+        assertEquals(1, fibonacciOf(1));
+        assertEquals(1, fibonacciOf(2));
+        assertEquals(2, fibonacciOf(3));
+        assertEquals(8, fibonacciOf(6));
     }
 }
